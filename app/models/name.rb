c994d45397a87a38e15037ac109440b9078370cb
Tyler Hughes <iampbt@gmail.com>
2011-11-11 22:28:37 -0500

    updated layout

--- app/models/name.rb
+++ app/models/name.rb
@@ -3,6 +3,8 @@ class Name < ActiveRecord::Base
 	validates_uniqueness_of :name
 
 	attr_accessible :name, :votes, :status
+	will_paginate @names, :param_name => :names_page
+	will_paginate @comments, :param_name => :comments_page
 
 	has_many :comments
 end
